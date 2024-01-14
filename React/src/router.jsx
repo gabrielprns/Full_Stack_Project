c735e3af6bcd84ec7fb92a5fce6584b1d6dec25c
gabrielprns <64814557+gabrielprns@users.noreply.github.com>
2024-01-14 14:50:00 -0300

    Full-Stack-Project

---
+++ React/src/router.jsx
@@ -0,0 +1,60 @@
+import { createBrowserRouter } from "react-router-dom";
+import Login from "./views/Login";
+import SignUp from "./views/SignUp";
+import Users from "./views/Users";
+import NotFound from "./views/NotFound";
+import DefaultLayout from "./components/DefaultLayout";
+import GuestLayout from "./components/GuestLayout";
+import DashBoard from "./views/DashBoard";
+
+const router = createBrowserRouter([
+
+  {
+    path: "/",
+    element: <DefaultLayout/>,
+    children:[
+
+      {
+        path: "/",
+        element: <Users/>
+      },
+
+      {
+        path: "/DashBoard",
+        element: <DashBoard/>
+      },
+
+      {
+        path: "/Users",
+        element: <Users/>
+      }
+
+    ]
+  },
+
+
+  {
+    path: "/",
+    element: <GuestLayout/>,
+    children:[
+      {
+        path: "/Login",
+        element: <Login/>
+      },
+
+      {
+        path: "/SignUp",
+        element: <SignUp/>
+      }
+    ]
+  },
+  
+  
+  {
+    path: "*",
+    element: <NotFound/>
+  },
+
+])
+
+export default router;
