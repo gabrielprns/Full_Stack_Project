0375baf2c877f1fdabc3720ab5389e08d277d0da
gabrielprns <64814557+gabrielprns@users.noreply.github.com>
2024-02-07 19:28:22 -0300

    Console.log

--- React/src/views/SignUp.jsx
+++ React/src/views/SignUp.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import {useRef}from 'react'
 import { Link } from 'react-router-dom'
 
 const SignUp = () => {
+  const nameRef = useRef();
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const passwordConfirmationRef = useRef();
 
   const onSubmit = (ev) => {
     ev.preventDefault()
+    const payload = {
+    name:nameRef.current.value,
+    email:emailRef.current.value,
+    password:passwordRef.current.value,
+    password_confirmation:passwordConfirmationRef.current.value
+    }
+
+    console.log(payload);
   }
   return (
     <div className="login-signup-form animated fadeInDown">
@@ -13,10 +25,10 @@ const SignUp = () => {
         <h1 className="title">
           Sign Up for free
         </h1>
-        <input type="email" placeholder="Full Name" />
-        <input type="email" placeholder="Email Address" />
-        <input type="password" placeholder="Password" />
-        <input type="password" placeholder="Confirm Password" />
+        <input ref={nameRef} placeholder="Full Name" />
+        <input ref={emailRef} type="email" placeholder="Email Address" />
+        <input ref={passwordRef} type="password" placeholder="Password" />
+        <input ref={passwordConfirmationRef} type="password" placeholder="Confirm Password" />
         <button className="btn btn-block">Signup</button>
         <p className='message'>
           Already Registered? <Link to="/login"> Login</Link>
